5bb2752e989f844aced227651967a8c812437fdf
Colin Walters <walters@verbum.org>
2020-11-20 15:47:52 +0000

    Work in qemu, various fixes

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@
 //! and redirect desired ephemeral paths to it.
 //! https://github.com/coreos/ignition/issues/1126
 
-use std::path::Path;
 use anyhow::{anyhow, bail, Context, Result};
-use std::process::Command;
+use openat_ext::OpenatDirExt;
 use serde_derive::Deserialize;
 use std::borrow::Cow;
 use std::fs::create_dir;
+use std::path::Path;
+use std::process::Command;
 
 const CONFIG_PATH: &str = "/etc/coreos-cloud-instance-store-provisioner.yaml";
 const MOUNTPOINT: &str = "/var/mnt/instance-storage";
@@ -86,16 +87,17 @@ mod nvme {
     use super::*;
 
     #[derive(Debug, Deserialize)]
-    #[serde(rename_all="PascalCase")]
+    #[serde(rename_all = "PascalCase")]
     struct DevicesOutput {
         devices: Vec<Device>,
     }
 
     #[derive(Debug, Deserialize)]
-    #[serde(rename_all="PascalCase")]
+    #[serde(rename_all = "PascalCase")]
     pub(crate) struct Device {
         pub(crate) device_path: String,
         pub(crate) model_number: String,
+        pub(crate) serial_number: String,
     }
 
     pub(crate) fn list() -> Result<Vec<Device>> {
@@ -114,17 +116,18 @@ mod lvm {
     use super::*;
 
     fn pvcreate(dev: &str) -> Result<()> {
-        Command::new("lvm")
-            .arg("pvcreate")
-            .arg(dev)
-            .run()
+        Command::new("lvm").arg("pvcreate").arg(dev).run()
     }
 
     fn escape(name: &str) -> String {
         name.replace('-', "--")
     }
 
-    pub(crate) fn new_striped_lv(lvname: &str, vgname: &str, devices: &Vec<String>) -> Result<String> {
+    pub(crate) fn new_striped_lv(
+        lvname: &str,
+        vgname: &str,
+        devices: &Vec<String>,
+    ) -> Result<String> {
         for dev in devices {
             pvcreate(&dev)?;
         }
@@ -150,13 +153,74 @@ mod aws {
     const INSTANCE_MODEL: &str = "Amazon EC2 NVMe Instance Storage";
 
     pub(crate) fn devices() -> Result<Vec<String>> {
-        Ok(nvme::list()?.into_iter().filter_map(|dev| {
-            if dev.model_number == INSTANCE_MODEL {
-                Some(dev.device_path)
-            } else {
-                None
-            }
-        }).collect())
+        Ok(nvme::list()?
+            .into_iter()
+            .filter_map(|dev| {
+                if dev.model_number == INSTANCE_MODEL {
+                    Some(dev.device_path)
+                } else {
+                    None
+                }
+            })
+            .collect())
+    }
+}
+
+// This one is totally made up for local testing; use e.g.
+// cosa run --bind-ro /var/srv/walters/src/github/cgwalters/coreos-cloud-instance-store-provisioner/,/run/workdir -- -device nvme,drive=one,serial=CoreOSQEMUInstance1
+// -drive if=none,id=one,file=/tmp/empty.qcow2  -device nvme,drive=two,serial=CoreOSQEMUInstance2 -drive if=none,id=two,file=/tmp/empty2.qcow2
+mod qemu {
+    use super::*;
+
+    const PREFIX: &str = "CoreOSQEMUInstance";
+
+    pub(crate) fn devices() -> Result<Vec<String>> {
+        Ok(nvme::list()?
+            .into_iter()
+            .filter_map(|dev| {
+                if dev.serial_number.starts_with(PREFIX) {
+                    Some(dev.device_path)
+                } else {
+                    None
+                }
+            })
+            .collect())
+    }
+}
+
+mod systemd {
+    use super::*;
+    use libsystemd::unit;
+    use std::io::Write as IoWrite;
+
+    pub(crate) fn write_mount_unit(
+        what_path: &str,
+        where_path: &str,
+        mnt_type: &str,
+    ) -> Result<String> {
+        let dir = openat::Dir::open("/etc/systemd/system")?;
+        let name = format!("{}.mount", unit::escape_path(where_path));
+        dir.write_file_with(&name, 0o644, |f| -> Result<()> {
+            write!(
+                f,
+                r##"[Unit]
+Before=local-fs.target
+
+[Mount]
+What={what_path}
+Where={where_path}
+Type={mnt_type}
+
+[Install]
+WantedBy=local-fs.target
+"##,
+                what_path = what_path,
+                where_path = where_path,
+                mnt_type = mnt_type
+            )?;
+            Ok(())
+        })?;
+        Ok(name)
     }
 }
 
@@ -166,39 +230,51 @@ fn main() -> Result<()> {
         println!("No configuration specified.");
         return Ok(());
     }
-    let config: Config = serde_yaml::from_reader(std::io::BufReader::new(std::fs::File::open(configpath)?))?;
+    let config: Config =
+        serde_yaml::from_reader(std::io::BufReader::new(std::fs::File::open(configpath)?))?;
     if config.directories.is_empty() {
         bail!("Specified directories list is empty");
     }
     let ephemeral = match coreos::get_platform()?.as_str() {
-        "aws" => {
-            aws::devices()?
-        },
+        "aws" => aws::devices()?,
+        "qemu" => qemu::devices()?,
         other => {
             println!("Unhandled platform: {}", other);
-            return Ok(())
+            return Ok(());
         }
     };
     let dev = match ephemeral.len() {
         0 => {
-            println!("No ephemeral devices found");
+            println!("No ephemeral devices found.");
             return Ok(());
         }
-        1 => {
-            Cow::Borrowed(&ephemeral[0])
-        },
-        _ => {
-            Cow::Owned(lvm::new_striped_lv("ephemeral-striped", "coreos-vg", &ephemeral)?)
-        }
+        1 => Cow::Borrowed(&ephemeral[0]),
+        _ => Cow::Owned(lvm::new_striped_lv(
+            "striped",
+            "coreos-instance-vg",
+            &ephemeral,
+        )?),
     };
-    Command::new("mkfs.xfs").arg(dev.as_str()).run()?;
+    let dev = dev.as_str();
+    Command::new("mkfs.xfs").arg(dev).run()?;
     create_dir(MOUNTPOINT).context("creating mountpoint")?;
+    let mountunit = systemd::write_mount_unit(dev, MOUNTPOINT, "xfs")?;
+    Command::new("systemctl").arg("daemon-reload").run()?;
+    Command::new("systemctl")
+        .args(&["enable", "--now"])
+        .arg(&mountunit)
+        .run()?;
+    let root = openat::Dir::open("/")?;
     for d in config.directories.iter().map(Path::new) {
-        std::fs::remove_dir_all(d)?;
-        let name = d.file_name().ok_or_else(|| anyhow!("Expected filename in {:?}", d))?;
+        root.remove_all(d)?;
+        let name = d
+            .file_name()
+            .ok_or_else(|| anyhow!("Expected filename in {:?}", d))?;
         let target = Path::new(MOUNTPOINT).join(name);
         create_dir(&target).context("creating target dir")?;
-        std::os::unix::fs::symlink(&target, d).with_context(|| format!("creating symlink {:?}", d))?;
+        std::os::unix::fs::symlink(&target, d)
+            .with_context(|| format!("creating symlink {:?}", d))?;
+        println!("Set up {:?} to use instance storage", d);
     }
     Ok(())
 }
